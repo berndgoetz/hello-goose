6d0a4a1c857d600435c1d78604d6257d6c9f6669
Bernd Goetz <Bernd_Goetz@swissre.com>
2020-09-07 22:34:51 +0200

    println added

--- kotlin/src/main/kotlin/io/gooses/hellogoose/HellogooseApplication.kt
+++ kotlin/src/main/kotlin/io/gooses/hellogoose/HellogooseApplication.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping
 class HellogooseApplication
 
 fun main(args: Array<String>) {
+	println("Hello from Kotlin")
 	runApplication<HellogooseApplication>(*args)
 }
 
